b02bc23a3d02fd63631849bb39c6fd4a7e69c264
gcc virus & exploit <132490017+feedosssoff@users.noreply.github.com>
2025-11-25 15:21:35 +0300

    bubble_sort added

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from random import randint
+
 def bubble_sort(array):
-  pass
+  array_length = len(array)
+  for i in range(array_length-1):
+    for j in range(array_length-i-1):
+        if array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1], array[j]
+
+array = list() 
+length = randint(10, 40)
+for element in range(length):
+  array.append(randint(1,1000))
+
+print(bubble_sort(array)) 
+
+ 
