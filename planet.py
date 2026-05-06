bb5531267d34ac88f32b3d5fea37c5799051a2fd
gcc virus & exploit <132490017+feedosssoff@users.noreply.github.com>
2026-05-06 23:13:54 +0300

    ~ final version of planet.py

--- planet.py
+++ planet.py
@@ -1,52 +1,60 @@
-class Planet():
-    total_planets = 0 
-    def __init__(self, name, radius, mass, distance, type):
+class Planet:
+    count = 0
+
+    def __init__(self, name, radius, mass, distance, ptype):
+        Planet.count += 1
         self.name = name
         self.__radius = radius
         self.__mass = mass
         self.distance = distance
-        self.__type = type
-        Planet.total_planets += 1
-        
+        self.ptype = ptype
+        print(f"Создана планета {self.name}")
+
     @property
     def radius(self):
         return self.__radius
-    
+
     @radius.setter
-    def radius(self, rad):
-        if rad > 0:
-            self.__radius = rad
+    def radius(self, value):
+        if value > 0:
+            self.__radius = value
         else:
             raise ValueError("Радиус должен быть положительным числом")
-        
+
     @property
     def mass(self):
         return self.__mass
-    
+   
     @mass.setter
-    def mass(self, mas):
-        if mas > 0:
-            self.__mass = mas
+    def mass(self, value):
+        if value > 0:
+            self.__mass = value
         else:
             raise ValueError("Масса должна быть положительным числом")
-            
-    @property
-    def type(self):
-        return self.__type
-    
-    @type.setter
-    def type(self, value):
-        if value in ["газовый гигант", "каменная", "ледяной гигант"]:
-            self.__type = value
-        else:
-            raise ValueError("Тип должен быть 'газовый гигант', 'каменная' или 'ледяной гигант'")
-        
-    def transform(self):
+
+    def __str__(self):
+        return f"{self.name}:\nРадиус - {self.radius}\nМасса - {self.mass}\nРасстояние от Солнца - {self.distance}\nТип планеты - {self.ptype}"
+
+    def __repr__(self):
+        return f"Planet('{self.name}', {self.radius}, {self.mass}, {self.distance}, '{self.ptype}')"
+
+    def __del__(self):
+        print(f"Удаление {self.name}")
+
+    def __copy__(self):
+        return Planet(self.name, self.radius, self.mass, self.distance, self.ptype)
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def to_dict(self):
         return {
-            "Имя планеты": self.name,
-            "Радиус": self.radius,
-            "Масса": self.mass,
-            "Расстояние от Солнца": self.distance,
-            "Тип планенты": self.type
+            "name": self.name,
+            "radius": self.radius,
+            "mass": self.mass,
+            "distance": self.distance,
+            "type": self.ptype
         }
-    
